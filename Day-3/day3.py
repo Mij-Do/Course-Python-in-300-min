acf842350a07be116924410223fcece4999ac54d
ahmed <ahmedmijdo2@gmail.com>
2025-12-07 14:42:22 +0200

    Day-4 Variables

--- Day-3/day3.py
+++ Day-3/day3.py
@@ -1,3 +1,4 @@
+# you can use the print() function to display text or output values
 # we here use print() function to print a pyramid of stars
 print("*");
 print("**");
@@ -5,4 +6,8 @@ print("***");
 print("****");
 print("*****");
 print("******");
+
+#  لو انت كتبت print وكنت سايب قبلها مسافة هيظهرلك error
+# Indentation Error: unexpected indent
+# معناه مسافة غير متوقعة
 
---
+++ Day-4/day4.py
@@ -0,0 +1,7 @@
+# Variables are containers for storing data values.
+name = "Ahmed";
+
+print(name);
+
+# If you print the variable before you initialized it 
+# NameError: name "name" is not Defined
