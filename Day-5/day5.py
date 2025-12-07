31e21729cef213347c14e8b9e08ebd93d364949b
ahmed <ahmedmijdo2@gmail.com>
2025-12-07 16:30:15 +0200

    Day-5 Variabels Names

---
+++ Day-5/day5.py
@@ -0,0 +1,23 @@
+# Variable Names
+# 1- اهم حاجة تسمية المتغير يدل علي القيمة اللي فيه .
+name = "Ahmed";
+print(name); # output ==> Ahmed
+
+#####################################################
+
+# 2- مينفعش تبدا اسم المتغير برقم (1name) ده غير صحيح وهيطلع error
+# SyntaxError: invalid decimal literal
+# بس ينفع يكون الرقم تاني خانة او اخر خانة في الكلمة (n2ame) (name1)
+
+# 1name = "Ahmed" ==> SyntaxError
+n2ame = "Ahmed"; # ==> Ahmed
+name2 = "Ahmed"; # ==> Ahmed
+
+######################################################
+
+# 3- لو كان عندى أكتر من كلمة في المتغير 
+#  1- استخدم (_) 
+name_of_student = "Ahmed" # ==> Ahmed
+
+# 2- استخدم ال camelCase
+nameOfStudent = "Ahmed" # ==> Ahmed
