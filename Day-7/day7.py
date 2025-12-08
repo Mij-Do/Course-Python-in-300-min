d05d84a8ac3a068f83310524a4b14ba18f5556f0
ahmed <ahmedmijdo2@gmail.com>
2025-12-08 16:04:42 +0200

    Day-7 Data Types

---
+++ Day-7/day7.py
@@ -0,0 +1,16 @@
+# Data Types
+
+# 1- Integer 
+# ده بيخزن ارقام صحيحة زي 1,2,3
+int = 3;
+print(type(int)) # ==> integer
+
+# 2- Float
+# ده بيخزن ارقام برضو ولكن بتكون ارقام عشرية زي 1.5
+float = 1.5;
+print(type(float)) # ==> Float
+
+# 3- String
+# ده بيخزن نصوص زي "Ahmed"
+string = "Ahmed";
+print(type(string)) # ==> String
