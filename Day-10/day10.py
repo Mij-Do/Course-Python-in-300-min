c295d3738d19256022a6e687cd3a3058ca47855b
ahmed <ahmedmijdo2@gmail.com>
2025-12-11 14:09:10 +0200

    Day-10 Convert int into str

---
+++ Day-10/day10.py
@@ -0,0 +1,11 @@
+# convert integers into string
+
+age = 24;
+
+#print("my age is: " + age) 
+# ==> هنا هيطلع error عشان مينفعش اجمع بين Integer و String 
+
+# وعشان نحل المشكلة دي عندنا function في python بتحول ال integer الي string
+# str()
+
+print("my age is: " + str(age)) # ==> my age is: 24;
