0a9ff928b17ce10697b789a6bfadf3fb26505ce4
ahmed <ahmedmijdo2@gmail.com>
2025-12-11 13:52:18 +0200

    Day-9 Arithmetic operations

---
+++ Day-9/day9.py
@@ -0,0 +1,17 @@
+# Arithmetic operations
+# + - * / %
+print(10+8) # ==> 18
+print(10-8) # ==> 2
+print(10*8) # ==> 80 (*) دي معناه ضرب
+print(10/2) # 5 (/) دي معناها قسمة 
+
+# (%) Modulus
+# num % same num = zero 0
+print(100%100) # ==> 0
+
+# small num % big num = small num
+print(100%101) # ==> 100
+
+# big num % small num = big num - small num 
+print(30%6) # ==> 0
+# 30 - 6 = 24 - 6 = 18 - 6 = 12 - 6 = 6 -6 = 0
