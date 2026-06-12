77dc51d2e37d911c7e19a378a74f5063171b5de4
ahmed <ahmedmijdo2@gmail.com>
2026-06-12 03:06:16 +0300

    frist calc app in python

---
+++ Day-12/day12.py
@@ -0,0 +1,14 @@
+# my first calc app in python 
+print("| Welcome to Calculator App |");
+
+firstNum = float(input("First Number: "));
+secondNum = float(input("Second Number: "));
+
+print("| --------------------------- |");
+
+print("Adding: " + str(firstNum + secondNum));
+print("Subtract: " + str(firstNum - secondNum));
+print("Multiply: " + str(firstNum * secondNum));
+print("Divide: " + str(firstNum / secondNum));
+print("Modulus: " + str(firstNum % secondNum));
+
