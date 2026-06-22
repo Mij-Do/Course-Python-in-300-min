6dd6b686cd418079a0347d88aaeb0bd5831583f5
ahmed <ahmedmijdo2@gmail.com>
2026-06-23 02:28:30 +0300

    Day-13

---
+++ Day-13/day13.py
@@ -0,0 +1,27 @@
+# ((==)) => هل يساوي 
+# (> <) => اكبر من واصغر من 
+# (>=  <=) => اكبر واصغر من اوي يساوي
+# (!=) => لا يساوي
+
+result1 = (10 == 10); # true
+result2 = (10 == 11); # false
+
+result3 = (10 > 10); # false
+result4 = (10 < 10); # false 
+result5 = (10 >= 10); # true 
+result6 = (10 <= 10); # true 
+
+result7 = (10 != 10); # false
+
+print(result1);
+print(result2);
+print(result3);
+print(result4);
+print(result5);
+print(result6);
+print(result7);
+
+
+
+
+
